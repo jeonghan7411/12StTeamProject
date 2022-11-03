c2f434a14b5e83c69e6547037512e1f9521dd8ed
LeeGukHeon <lkhcode@naver.com>
2022-11-03 16:11:12 +0900

    add-login-placeholder

--- client/src/components/UI/Input.jsx
+++ client/src/components/UI/Input.jsx
@@ -2,7 +2,16 @@ import React from "react";
 
 import classes from "./Input.module.css";
 
-const Input = ({ type, id, value, onChange, onBlur, isValid, className }) => {
+const Input = ({
+  type,
+  id,
+  value,
+  onChange,
+  onBlur,
+  isValid,
+  className,
+  placeholder,
+}) => {
   return (
     <div
       className={`${classes.input} ${className} ${
@@ -16,6 +25,7 @@ const Input = ({ type, id, value, onChange, onBlur, isValid, className }) => {
         value={value}
         onChange={onChange}
         onBlur={onBlur}
+        placeholder={placeholder}
       />
     </div>
   );
