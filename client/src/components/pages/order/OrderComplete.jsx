e545dd8d6a4165f8c0d85ee91825617ac1ae687f
LeeGukHeon <lkhcode@naver.com>
2022-11-08 23:24:56 +0900

    STYLE cart, order

---
+++ client/src/components/pages/order/OrderComplete.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import classes from "./OrderComplete.module.css";
+const OrderComplete = () => {
+  return (
+    <React.Fragment>
+      <div>가운데로 정렬 여기에</div>
+    </React.Fragment>
+  );
+};
+
+export default OrderComplete;
--- client/src/components/pages/productCart/ProductCart.jsx
+++ client/src/components/pages/productCart/ProductCart.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import classes from "./ProductCart.module.css";
+import { useNavigate } from "react-router-dom";
 const ProductCart = () => {
+  const navigate = useNavigate();
   return (
     <div className={classes["productcart-wrapper"]}>
       <div className={classes["productcart-cart"]}>
@@ -126,8 +128,39 @@ const ProductCart = () => {
         </div>
       </div>
       <div className={classes["productcart-order"]}>
-        <div>적립혜택</div>
-        <div>결제 예정금액, 상품금액, 할인금액, 합계, 주문버튼</div>
+        <div className={classes["productcart-order-point"]}>
+          <h3>적립혜택</h3>
+          <p>적립 혜택이 없습니다.</p>
+        </div>
+        <div className={classes["productcart-order-purchase"]}>
+          <h3>결제 예정금액</h3>
+          <div className={classes["productcart-order-purchase-product"]}>
+            <h4>상품금액</h4>
+            <p>
+              <strong>0</strong>원
+            </p>
+          </div>
+          <div className={classes["productcart-order-purchase-discount"]}>
+            <h4>할인금액</h4>
+            <p>
+              <strong>0</strong>원
+            </p>
+          </div>
+          <div className={classes["productcart-order-purchase-total"]}>
+            <h4>합계</h4>
+            <p>
+              <strong style={{ fontSize: "20px" }}>0</strong>원
+            </p>
+          </div>
+          <div className={classes["productcart-order-purchase-btn"]}>
+            {/* 여기 주문하기 온클릭 링크 나중에 노드로 바꾸고 노드에서 리다렉 */}
+            <input
+              type={"button"}
+              value="주문하기"
+              onClick={() => navigate("/order")}
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
