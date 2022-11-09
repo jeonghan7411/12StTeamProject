1000984a1d14a64bcbe7fe0d6cb02a0cd18f3a0b
LeeGukHeon <lkhcode@naver.com>
2022-11-09 13:49:11 +0900

    FEAT server

--- server/server.js
+++ server/server.js
@@ -8,15 +8,70 @@ const db = require("./db/db");
 const fs = require("fs");
 const app = express();
 
+const convert = require("xml-js");
+const request = require("request");
 // middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static("uploads"));
-
 //나중에 멀터 업로드 처리
 
 // url
 
+//네이버 api 받아와서 db에 넣은 흔적
+/*
+let data = [];
+
+let client_id = "wuWicrMH0NCEgX2yYL1s";
+let client_secret = "NqTst4k3ME";
+
+app.get("/search/shop", (req, res) => {
+  let api_url =
+    "https://openapi.naver.com/v1/search/shop.json?query=" +
+    encodeURI(req.query.query) +
+    "&display=50&start=1001";
+  var options = {
+    url: api_url,
+    headers: {
+      "X-Naver-Client-Id": client_id,
+      "X-Naver-Client-Secret": client_secret,
+    },
+  };
+  request.get(options, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      res.writeHead(200, { "Content-Type": "text/json;charset=utf-8" });
+      res.end(body);
+      let resres = JSON.parse(body);
+      data = resres.items;
+      let sql = "INSERT INTO products VALUES(null, ?, ?, ?, ?, ?, ?, ?, ?, ?);";
+      for (let i = 0; i < data.length; i++) {
+        db.query(
+          sql,
+          [
+            data[i]["productId"],
+            data[i]["title"],
+            data[i]["image"],
+            data[i]["lprice"],
+            data[i]["mallName"],
+            data[i]["category1"],
+            data[i]["category2"],
+            data[i]["category3"],
+            data[i]["category4"],
+          ],
+          (err) => {
+            if (err) throw err;
+          }
+        );
+      }
+    } else {
+      res.status(response.statusCode).end();
+      console.log("error = " + response.statusCode);
+    }
+  });
+});
+
+// https://openapi.11st.co.kr/openapi/OpenApiService.tmall?key=0c2e778ddcaff57dd5cdc2a2d1c91894&apiCode=ProductSearch&keyword=&option=Categories
+*/
 // listen
 app.listen(process.env.PORT, () => {
   let folder = "uploads";
