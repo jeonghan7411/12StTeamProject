5164f2d16eff22b0db99e7fa6e3860c4aee79ce3
jeonghan7411 <aufkrh@naver.com>
2022-11-11 16:05:30 +0900

    addr api

---
+++ client/src/components/layout/AddressModal.jsx
@@ -0,0 +1,35 @@
+import DaumPostcode from "react-daum-postcode";
+import Modal from "../UI/Modal";
+
+const AddressModal = ({ onClose, setInputAddr, setInputZipCode }) => {
+  const handleComplete = (data) => {
+    let fullAddress = data.address;
+    let extraAddress = "";
+    // console.log(data);
+    if (data.addressType === "R") {
+      if (data.bname !== "") {
+        extraAddress += data.bname;
+      }
+      if (data.buildingName !== "") {
+        extraAddress +=
+          extraAddress !== "" ? `, ${data.buildingName}` : data.buildingName;
+      }
+      fullAddress += extraAddress !== "" ? ` (${extraAddress})` : "";
+    }
+    onClose();
+    setInputZipCode(data.zonecode);
+    setInputAddr(fullAddress); // e.g. '서울 성동구 왕십리로2길 20 (성수동1가)'
+  };
+
+  const handleSearch = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <Modal onClose={onClose}>
+      <DaumPostcode onComplete={handleComplete} onSearch={handleSearch} />
+    </Modal>
+  );
+};
+
+export default AddressModal;
